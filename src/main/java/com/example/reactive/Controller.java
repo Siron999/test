1ac716b77ceaf42e0d0cbcd045ce848936afad9b
Siron <sironshakya10@gmail.com>
2022-10-28 19:25:46 +0545

    d2

--- src/main/java/com/example/reactive/Controller.java
+++ src/main/java/com/example/reactive/Controller.java
@@ -18,10 +18,11 @@ public class Controller {
         List<Integer> ids = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14));
         System.out.println("Main" + Thread.currentThread().getName());
         Flux.fromIterable(ids).flatMap((x) -> {
-            System.out.println("test1");
+            System.out.println("test");
             return getData(x);
         }).subscribe();
         System.out.println("TEst main");
+        System.out.println("hello");
         return null;
     }
 
